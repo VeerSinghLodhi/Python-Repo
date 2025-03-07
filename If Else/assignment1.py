6d46ae1005d6a10b0d6696a35f6741204789dfd3
Veer Singh Lodhi <veerlodhi54@gmail.com>
2025-03-07 21:41:01 +0530

    Delete If Else directory

--- If Else/assignment1.py
+++
@@ -1,9 +0,0 @@
-
-num=int(input("Enter a number : "))
-
-if num>0:
-    print(num,"Is Positive")
-elif num<0:
-    print(num,"Is Negative")
-else:
-        print(num,"Is Zero")
--- If Else/if1.py
+++
@@ -1,8 +0,0 @@
-
-age=int(input("Enter your age : "))
-if  age>=18:
-    print("Congratulations!")
-    print("You can vote")
-else:
-    print("Sorry, you are not allowed to vote")
-    print("You can not vote")
